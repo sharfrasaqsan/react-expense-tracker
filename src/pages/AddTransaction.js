37acae80343ffd320a949222a9cf27f8804001d3
Mohamed Sharfiras <133948569+Mohamed-Sharfiras@users.noreply.github.com>
2025-07-15 10:21:22 +0530

    balance updated

--- src/pages/AddTransaction.js
+++ src/pages/AddTransaction.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import DataContext from "../context/DataContext";
 import request from "../api/request";
 import { format } from "date-fns";
